9d46398dc8cbf66d8b55b7732fbdb98b6cdd075f
RobertGumpert <walkmanmail19@gmail.com>
2021-04-12 17:55:13 +0300

    Обновление пакета textClearing

--- textPreprocessing/textClearing/clearing-text.go
+++ textPreprocessing/textClearing/clearing-text.go
@@ -14,11 +14,11 @@ import (
 )
 
 const (
-	codeRegex        = `(?i)[\w\d]+[.](?i)[\w\d]+[(](?i)[\w\d]{0,}[)]`
-	asciiRegex       = `[[:^ascii:]]`
-	symbolsRegex     = `[]\d%:$"';[&*=<>}{)(?!/.,\-_^@]`
-	specialWordRegex = `(?i)([_\-&*:;#<>@""''=/+\d~^%]{0,}\w+[_\-&*:;#<>@""''=/+\d~^%]{1,}\w{0,})`
-	urlRegex         = `(?i)https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
+	CodeRegex        = `(?i)[\w\d]+[.](?i)[\w\d]+[(](?i)[\w\d]{0,}[)]`
+	AsciiRegex       = `[[:^ascii:]]`
+	SymbolsRegex     = `[]\d%:$"';[&*=<>}{)(?!/.,\-_^@]`
+	SpecialWordRegex = `(?i)([_\-&*:;#<>@""''=/+\d~^%]{0,}\w+[_\-&*:;#<>@""''=/+\d~^%]{1,}\w{0,})`
+	UrlRegex         = `(?i)https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
 )
 
 type Contains func(str *string) error
@@ -50,24 +50,28 @@ func containsByRegex(regex string, str *string) error {
 //
 
 func ContainsCode(str *string) error {
-	return containsByRegex(codeRegex, str)
+	return containsByRegex(CodeRegex, str)
 }
 
 func ContainsASCII(str *string) error {
-	return containsByRegex(asciiRegex, str)
+	return containsByRegex(AsciiRegex, str)
 }
 
 func ContainsSymbols(str *string) error {
-	return containsByRegex(symbolsRegex, str)
+	return containsByRegex(SymbolsRegex, str)
 }
 
 func ContainsSpecialWord(str *string) error {
-	return containsByRegex(specialWordRegex, str)
+	return containsByRegex(SpecialWordRegex, str)
 }
 
 //
 //--------------------------------------Clear------------------------------------------------------------------------
 //
+
+func ClearByRegex(str *string, regex string) {
+	clearByRegex(regex, str)
+}
 
 func ClearMarkdown(str *string) {
 	bts := blackfriday.Run([]byte(*str))
@@ -77,23 +81,31 @@ func ClearMarkdown(str *string) {
 	clearText = strip.StripTags(clearText)
 	fmt.Println(clearText)
 	clearText = strings.ReplaceAll(clearText, " !*! ", "\n")
-	*str = clearText
+	slice := ToSlice(&clearText)
+	s := make([]string, 0)
+	for _, w := range *slice {
+		if !strings.Contains(w, "&") {
+			w = strings.TrimSpace(w)
+			s = append(s, w)
+		}
+	}
+	*str = strings.Join(s, " ")
 }
 
 func ClearASCII(str *string) {
-	clearByRegex(asciiRegex, str)
+	clearByRegex(AsciiRegex, str)
 }
 
 func ClearCode(str *string) {
-	clearByRegex(codeRegex, str)
+	clearByRegex(CodeRegex, str)
 }
 
 func ClearSymbols(str *string) {
-	clearByRegex(symbolsRegex, str)
+	clearByRegex(SymbolsRegex, str)
 }
 
 func ClearSpecialWord(str *string) {
-	clearByRegex(specialWordRegex, str)
+	clearByRegex(SpecialWordRegex, str)
 }
 
 func ClearSingleCharacters(str *string) error {
@@ -152,39 +164,3 @@ func GetLemmas(str *string, deleteWordsWithoutLemma bool, lemmatizers ...*golem.
 	return &lemmas
 }
 
-type DoClear func(str *string) (*string, *[]string, error)
-
-func CustomClear(deleteWordsWithoutLemma bool, lemmatizer *golem.Lemmatizer, stopRegex []Contains, clearRegex []Clear) DoClear {
-	return func(str *string) (*string, *[]string, error) {
-		if len(*str) == 0 {
-			return nil, nil, errors.New("String is empty. ")
-		}
-		if stopRegex != nil {
-			for _, stop := range stopRegex {
-				if err := stop(str); err != nil {
-					return nil, nil, err
-				}
-			}
-		}
-		if clearRegex != nil {
-			for _, clear := range clearRegex {
-				clear(str)
-			}
-		}
-		clearText := stopwords.CleanString(*str, "en", true)
-		clearByRegex(urlRegex, &clearText)
-		slice := GetLemmas(&clearText, deleteWordsWithoutLemma, lemmatizer)
-		clearText = strings.Join(*slice, " ")
-		if err := ClearSingleCharacters(&clearText); err != nil {
-			return nil, nil, err
-		}
-		slice = ToSlice(&clearText)
-		if len(clearText) == 0 {
-			return nil, nil, errors.New("Clear string is empty. ")
-		}
-		if len(*slice) == 1 {
-			return nil, nil, errors.New("Clear string is with 1 element. ")
-		}
-		return &clearText, slice, nil
-	}
-}
